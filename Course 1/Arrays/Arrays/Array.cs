3fbcd279a098fffcc6aaa4a49f9e81f1a850f383
Chris <52719722+blue1317@users.noreply.github.com>
2021-10-20 20:11:00 -0400

    Arrays

---
+++ Course 1/Arrays/Arrays/Array.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+
+    // Runtime Complexity of this Class: O(n)
+    
+    public class Array
+    {
+        private int[] items;
+        private int count;
+        public Array(int size)
+        {
+            items = new int[size];
+
+        }
+
+        public void Print()
+        {
+            for (var i = 0; i < count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+
+        public void Insert(int value)
+        {
+            if (items.Length == count)
+            {
+                var newArray = new int[count * 2];
+                for (var i = 0; i < count; i++)
+                {
+                    newArray[i] = items[i];
+                }
+
+                items = newArray;
+            }
+
+            items[count++] = value;
+        }
+
+        public void InsertAt(int value, int index)
+        {
+            if (items.Length == count)
+            {
+                var newArray = new int[count * 2];
+                for (var i = 0; i < count; i++)
+                {
+                    newArray[i] = items[i];
+                }
+
+                items = newArray;
+            }
+
+            for (int i = index; i < count; i++)
+            {
+                items[i + 1] = items[i];
+                items[i] = value;
+                
+            }
+            
+            count++;
+            
+        }
+
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new ArgumentException();
+
+            for (int i = index; i < count; i++)
+                items[i] = items[i + 1];
+            
+            count--;
+        }
+
+        public int IndexOf(int value)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int LargestNumber()
+        {
+            return items.Max();
+        }
+    }
+}
---
+++ Course 1/Arrays/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new Array(3);
+            
+
+            numbers.Insert(10);
+            numbers.Insert(20);
+            numbers.Insert(30);
+            numbers.Insert(40);
+            numbers.RemoveAt(2);
+            numbers.Insert(50);
+           
+            numbers.InsertAt(80,3);
+
+            numbers.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("Index of: " + numbers.IndexOf(20));
+            Console.WriteLine("Index of: " + numbers.IndexOf(200));
+            Console.WriteLine("Largest number: " + numbers.LargestNumber());
+        }
+    }
+}
